549bd06e043594d39b02fedd2e5184ac2b9255c2
TOLSTbIU88 <gamer-9600@yandex.ru>
2022-11-27 15:34:44 +0300

    start

--- src/main/java/org/example/controller/MyControl.java
+++ src/main/java/org/example/controller/MyControl.java
@@ -3,6 +3,9 @@ package org.example.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class MyControl {
 
+    @RequestMapping(path = "/test", method = RequestMethod.GET)
+    public String test() {
+        log.info("Работает, Ура!");
+        return "Ура, работает!";
+    }
 
+    @PostMapping(path = "/test")
+    public String test2() {
+        return "Постмаппинг работает!";
+    }
 }
