7e070f6bccd3e56934afab792288f8c7f5fca9bc
TOLSTbIU88 <gamer-9600@yandex.ru>
2022-11-27 15:30:43 +0300

    start

---
+++ src/main/java/org/example/controller/MyControl.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class MyControl {
+
+
+}
